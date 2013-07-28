be3058d54df690245343b3dca5c671ad8291abbb
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-07-29 10:03:10 +1200

    Task description

--- editor/initial_code/python_32.py
+++ editor/initial_code/python_32.py
@@ -1,8 +1,26 @@
 def checkio(data):
-
     #replace this for solution
-    return None
+    return [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
-    pass
+    print(checkio([
+        [2, 7, 6],
+        [9, 5, 1],
+        [4, 3, 0]
+    ]))
+    #must return [[2, 7, 6], [9, 5, 1], [4, 3, 8]]
+
+    print(checkio([
+        [0, 0, 0],
+        [0, 5, 0],
+        [0, 0, 0]
+    ]))
+    #can return [[2, 7, 6], [9, 5, 1], [4, 3, 8]] or
+    # [[4, 9, 2], [3, 5, 7], [8, 1, 6]
+
+    print(checkio([[1, 15, 14, 4],
+                   [12, 0, 0, 9],
+                   [8, 0, 0, 5],
+                   [13, 3, 2, 16]]))
+    # answer [[1, 15, 14, 4], [12, 6, 7, 9], [8, 10, 11, 5], [13, 3, 2, 16]]
